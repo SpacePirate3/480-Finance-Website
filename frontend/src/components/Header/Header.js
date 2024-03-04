80a80c720a317e096149cedf2710e513a2fbd698
Michael Barry <BarryMichaelR@outlook.com>
2024-03-03 20:09:53 -0800

    Fixed breaking bugs

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,37 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Header.css';
-import { Link } from "react-router-dom";
-import Menu from './menu/menu.js';
+import {Link} from "react-router-dom";
 
 function Header() {
-    // Define the state hook directly inside the functional component
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-    };
-
     return (
         <div className="backgroundRectangle">
             <Link to={"/"}>
-                <div className="logo">
-                    <div className="graphic">
-                        <div className="rectangle rectangle1"></div>
-                        <div className="rectangle rectangle2"></div>
-                        <div className="rectangle rectangle3"></div>
-                    </div>
-                    <div>
-                        <div className="text textGlance">AT-A-GLANCE</div>
-                        <div className="text textFinance">FINANCE</div>
-                    </div>
+            <div className="logo">
+                <div className="graphic">
+                    {/* Rectangles are ordered from smallest to largest, bottom to top */}
+                    <div className="rectangle rectangle1"></div>
+                    <div className="rectangle rectangle2"></div>
+                    <div className="rectangle rectangle3"></div>
                 </div>
+                <div>
+                    <div className="text textGlance">AT-A-GLANCE</div>
+                    <div className="text textFinance">FINANCE</div>
+                </div>
+            </div>
             </Link>
             <div className="buttons">
                 <Link to={"/"}><div className="text homeButton">HOME</div></Link>
                 <div className="text homeButton">ABOUT US</div>
-                <button className="menuButton" onClick={toggleMenu}>Menu</button> {/* Menu button */}
+                <div className="hamburger-menu">
+                    <div className="hamburger"></div>
+                    <div className="hamburger"></div>
+                    <div className="hamburger"></div>
+                </div>
             </div>
-            {isMenuOpen && <Menu />} {/* Render the menu conditionally */}
         </div>
     );
 }
