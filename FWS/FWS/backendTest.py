09be23650acbfb30d9eda3d4f03a7ffd0e6723db
SteinerChris <101431294+SteinerChris@users.noreply.github.com>
2024-03-10 16:24:37 -0700

    Added test for historical latest call
    The backendTest.py file now tests the historical latest call, and if it returns the correct json fields, and if the historical has been updated within a day.

--- FWS/FWS/backendTest.py
+++ FWS/FWS/backendTest.py
@@ -2,7 +2,8 @@ import unittest
 from dotenv import load_dotenv
 import os
 import requests
-
+from datetime import date
+from datetime import datetime
 import mysql.connector
 import time
 
@@ -13,6 +14,8 @@ load_dotenv()
 
 
 class TestEnvironment(unittest.TestCase):
+    api = "http://localhost:8000/api/"
+    stocks = ['META', 'AMZN', 'AAPL', 'NFLX', 'GOOGL', 'DELL', 'INTC', 'MSFT', 'NVDA', 'IBM', 'AMD', 'HPQ', 'TSLA', 'TMUS', 'VZ']
     def test_3Keys(self):
         api_keys = [os.getenv("API_KEY1",False), os.getenv("API_KEY2",False), os.getenv("API_KEY3",False)]
         for key in api_keys:
@@ -31,8 +34,7 @@ class TestEnvironment(unittest.TestCase):
     def test_DbPort(self):
         self.assertEqual(os.getenv("DB_PORT",False),"3306")
     
-    api = "http://localhost:8000/api/"
-    stocks = ['META', 'AMZN', 'AAPL', 'NFLX', 'GOOGL', 'DELL', 'INTC', 'MSFT', 'NVDA', 'IBM', 'AMD', 'HPQ', 'TSLA', 'TMUS', 'VZ']
+    
 
     def test_api_list(self):
         resp = requests.get(self.api+"stock/list/")
@@ -43,7 +45,23 @@ class TestEnvironment(unittest.TestCase):
             self.assertEqual(resp.status_code,200)
             self.assertNotEqual(resp.json()['name'],'')
             self.assertEqual(resp.json()['symbol'],stock)
-
+            self.assertEqual(resp.json().keys(),['name','symbol'])
+    def test_api_historicalLatest(self):
+        for stock in self.stocks:
+            resp = requests.get(self.api+"stock/historical/latest/"+stock+'/')
+            #returns good http code
+            self.assertEqual(resp.status_code,200)
+            #the initial keys in the json object are correct
+            jsonKeys1 = list(resp.json().keys())
+            self.assertEqual(jsonKeys1,['model','pk','fields'])
+            #the keys of the value mapped to fields is correct
+            jsonKeys2 = list(resp.json()['fields'].keys())
+            self.assertEqual(jsonKeys2,['stock_id','date','open','high','low','close','volume'])
+            
+            #make sure historical has been updated within a day
+            reqDate =  datetime.strptime(resp.json()['fields']['date'], '%Y-%m-%d').date() 
+            today = date.today()
+            self.assertLessEqual((today-reqDate).days,1)
 
     
 
@@ -54,6 +72,6 @@ if __name__ == '__main__':
     api = "http://localhost:8000/api/"
     # resp = requests.get(api+"stock/list/")
     # print(resp.json())
-    resp = requests.get(api+"stock/overview/simple/"+'AMZN'+'/')
-    print(resp.json().keys())
-    #unittest.main()
+    #resp = requests.get(api+"stock/overview/simple/"+'AMZN'+'/')
+    #print(resp.json().keys())
+    unittest.main()
