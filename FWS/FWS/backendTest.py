a1139a37f01fbf5107334190251684bc0ce3980d
SteinerChris <101431294+SteinerChris@users.noreply.github.com>
2024-03-10 16:06:35 -0700

    Testing Api List and Api Simple
    Made tests for the api request for the stock list, and for the overview/simple

--- FWS/FWS/backendTest.py
+++ FWS/FWS/backendTest.py
@@ -26,10 +26,34 @@ class TestEnvironment(unittest.TestCase):
         self.assertNotEqual(os.getenv("DB_PASSWORD",False),False)
     def test_DbUser(self):
         self.assertNotEqual(os.getenv("DB_USER",False),False)
-    def test_(self):
+    def test_DbName(self):
         self.assertEqual(os.getenv("DB_NAME",False),"StockData")
-    def test_(self):
+    def test_DbPort(self):
         self.assertEqual(os.getenv("DB_PORT",False),"3306")
+    
+    api = "http://localhost:8000/api/"
+    stocks = ['META', 'AMZN', 'AAPL', 'NFLX', 'GOOGL', 'DELL', 'INTC', 'MSFT', 'NVDA', 'IBM', 'AMD', 'HPQ', 'TSLA', 'TMUS', 'VZ']
+
+    def test_api_list(self):
+        resp = requests.get(self.api+"stock/list/")
+        self.assertEqual(resp.json(),['META', 'AMZN', 'AAPL', 'NFLX', 'GOOGL', 'DELL', 'INTC', 'MSFT', 'NVDA', 'IBM', 'AMD', 'HPQ', 'TSLA', 'TMUS', 'VZ'])
+    def test_api_simple(self):
+        for stock in self.stocks:
+            resp = requests.get(self.api+"stock/overview/simple/"+stock+'/')
+            self.assertEqual(resp.status_code,200)
+            self.assertNotEqual(resp.json()['name'],'')
+            self.assertEqual(resp.json()['symbol'],stock)
+
+
+    
+
+   
+
 
 if __name__ == '__main__':
-    unittest.main()
+    api = "http://localhost:8000/api/"
+    # resp = requests.get(api+"stock/list/")
+    # print(resp.json())
+    resp = requests.get(api+"stock/overview/simple/"+'AMZN'+'/')
+    print(resp.json().keys())
+    #unittest.main()
