92a4ddb9d9377ac0da525c39c5048476d735f2db
SteinerChris <101431294+SteinerChris@users.noreply.github.com>
2024-03-10 17:30:36 -0700

    Subtests
    Sub tests make it so that each stock is treated as an individual test

--- FWS/FWS/backendTest.py
+++ FWS/FWS/backendTest.py
@@ -17,145 +17,170 @@ load_dotenv()
 class TestEnvironment(unittest.TestCase):
     api = "http://localhost:8000/api/"
     stocks = ['META', 'AMZN', 'AAPL', 'NFLX', 'GOOGL', 'DELL', 'INTC', 'MSFT', 'NVDA', 'IBM', 'AMD', 'HPQ', 'TSLA', 'TMUS', 'VZ']
-    def test_3Keys(self):
-        api_keys = [os.getenv("API_KEY1",False), os.getenv("API_KEY2",False), os.getenv("API_KEY3",False)]
-        for key in api_keys:
-            self.assertNotEqual(key,False)
+    # def test_3Keys(self):
+    #     api_keys = [os.getenv("API_KEY1",False), os.getenv("API_KEY2",False), os.getenv("API_KEY3",False)]
+    #     for key in api_keys:
+    #         self.assertNotEqual(key,False)
     
-    def test_DbHost(self):
-        self.assertNotEqual(os.getenv("DB_HOST",False),False)
-    def test_DbUser(self):
-        self.assertNotEqual(os.getenv("DB_USER",False),False)
-    def test_DbPW(self):
-        self.assertNotEqual(os.getenv("DB_PASSWORD",False),False)
-    def test_DbUser(self):
-        self.assertNotEqual(os.getenv("DB_USER",False),False)
-    def test_DbName(self):
-        self.assertEqual(os.getenv("DB_NAME",False),"StockData")
-    def test_DbPort(self):
-        self.assertEqual(os.getenv("DB_PORT",False),"3306")
+    # def test_DbHost(self):
+    #     self.assertNotEqual(os.getenv("DB_HOST",False),False)
+    # def test_DbUser(self):
+    #     self.assertNotEqual(os.getenv("DB_USER",False),False)
+    # def test_DbPW(self):
+    #     self.assertNotEqual(os.getenv("DB_PASSWORD",False),False)
+    # def test_DbUser(self):
+    #     self.assertNotEqual(os.getenv("DB_USER",False),False)
+    # def test_DbName(self):
+    #     self.assertEqual(os.getenv("DB_NAME",False),"StockData")
+    # def test_DbPort(self):
+    #     self.assertEqual(os.getenv("DB_PORT",False),"3306")
     
     
 
-    def test_api_list(self):
-        resp = requests.get(self.api+"stock/list/")
-        self.assertEqual(resp.json(),['META', 'AMZN', 'AAPL', 'NFLX', 'GOOGL', 'DELL', 'INTC', 'MSFT', 'NVDA', 'IBM', 'AMD', 'HPQ', 'TSLA', 'TMUS', 'VZ'])
-    def test_api_simple(self):
-        for stock in self.stocks:
-            resp = requests.get(self.api+"stock/overview/simple/"+stock+'/')
-            self.assertEqual(resp.status_code,200)
-            self.assertNotEqual(resp.json()['name'],'')
-            self.assertEqual(resp.json()['symbol'],stock)
-            self.assertEqual(resp.json().keys(),['name','symbol'])
-    def test_api_historicalLatest(self):
-        for stock in self.stocks:
-            resp = requests.get(self.api+"stock/historical/latest/"+stock+'/')
-            #returns good http code
-            self.assertEqual(resp.status_code,200)
-            #the initial keys in the json object are correct
-            jsonKeys1 = list(resp.json().keys())
-            self.assertEqual(jsonKeys1,['model','pk','fields'])
-            #the keys of the value mapped to fields is correct
-            jsonKeys2 = list(resp.json()['fields'].keys())
-            self.assertEqual(jsonKeys2,['stock_id','date','open','high','low','close','volume'])
-            
-            #make sure historical has been updated within a day
-            reqDate =  datetime.strptime(resp.json()['fields']['date'], '%Y-%m-%d').date() 
-            today = date.today()
-            self.assertLessEqual((today-reqDate).days,1)
-    def test_api_intradayLatest(self):
-        for stock in self.stocks:
-            resp = requests.get(self.api + "stock/intraday/latest/" + stock + '/')
+    # def test_api_list(self):
+    #     resp = requests.get(self.api+"stock/list/")
+    #     self.assertEqual(resp.json(),['META', 'AMZN', 'AAPL', 'NFLX', 'GOOGL', 'DELL', 'INTC', 'MSFT', 'NVDA', 'IBM', 'AMD', 'HPQ', 'TSLA', 'TMUS', 'VZ'])
+    # def test_api_simple(self):
+    #     for i, stock in zip(range(len(self.stocks)),self.stocks):
+    #         with self.subTest(i=i):
+    #             resp = requests.get(self.api+"stock/overview/simple/"+stock+'/')
+    #             self.assertEqual(resp.status_code,200)
+    #             self.assertNotEqual(resp.json()['name'],'')
+    #             self.assertEqual(resp.json()['symbol'],stock)
+    #             self.assertEqual(list(resp.json().keys()),['name','symbol'])
+    # def test_api_historicalLatest(self):
+    #     for i, stock in zip(range(len(self.stocks)),self.stocks):
+    #         with self.subTest(i=i):
+    #             resp = requests.get(self.api+"stock/historical/latest/"+stock+'/')
+    #             #returns good http code
+    #             self.assertEqual(resp.status_code,200)
+    #             #the initial keys in the json object are correct
+    #             jsonKeys1 = list(resp.json().keys())
+    #             self.assertEqual(jsonKeys1,['model','pk','fields'])
+    #             #the keys of the value mapped to fields is correct
+    #             jsonKeys2 = list(resp.json()['fields'].keys())
+    #             self.assertEqual(jsonKeys2,['stock_id','date','open','high','low','close','volume'])
+                
+    #             #make sure historical has been updated within a day
+    #             reqDate =  datetime.strptime(resp.json()['fields']['date'], '%Y-%m-%d').date() 
+    #             today = date.today()
+    #             self.assertLessEqual((today-reqDate).days,1)
+    # def test_api_intradayLatest(self):
+    #     for i, stock in zip(range(len(self.stocks)),self.stocks):
+    #         with self.subTest(i=i):
+    #             resp = requests.get(self.api + "stock/intraday/latest/" + stock + '/')
 
-            # Check HTTP status code
-            self.assertEqual(resp.status_code, 200)
+    #             # Check HTTP status code
+    #             self.assertEqual(resp.status_code, 200)
 
-            # Check the initial keys in the JSON object
-            json_keys1 = list(resp.json().keys())
-            self.assertEqual(json_keys1, ['model', 'pk', 'fields'])
+    #             # Check the initial keys in the JSON object
+    #             json_keys1 = list(resp.json().keys())
+    #             self.assertEqual(json_keys1, ['model', 'pk', 'fields'])
 
-            # Check the keys of the value mapped to fields
-            json_keys2 = list(resp.json()['fields'].keys())
-            self.assertEqual(json_keys2, ['stock_id', 'date', 'open', 'high', 'low', 'close', 'volume'])
+    #             # Check the keys of the value mapped to fields
+    #             json_keys2 = list(resp.json()['fields'].keys())
+    #             self.assertEqual(json_keys2, ['stock_id', 'date', 'open', 'high', 'low', 'close', 'volume'])
 
-            # Check if the date is within 2 hours of the current datetime in GMT
-            req_date_str = resp.json()['fields']['date']
-            req_date = datetime.strptime(req_date_str, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc)
+    #             # Check if the date is within 2 hours of the current datetime in GMT
+    #             req_date_str = resp.json()['fields']['date']
+    #             req_date = datetime.strptime(req_date_str, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc)
 
-            # Calculate the time difference
-            current_datetime_utc = datetime.utcnow().replace(tzinfo=timezone.utc)
-            time_difference = current_datetime_utc - req_date
+    #             # Calculate the time difference
+    #             current_datetime_utc = datetime.utcnow().replace(tzinfo=timezone.utc)
+    #             time_difference = current_datetime_utc - req_date
 
-            # Check if the time difference is within the allowed range (2 hours)
-            self.assertLessEqual(time_difference, timedelta(hours=2))
+    #             # Check if the time difference is within the allowed range (2 hours)
+    #             self.assertLessEqual(time_difference, timedelta(hours=2))
 
-    def test_api_intradayChart(self):
-        for stock in self.stocks:
-            resp = requests.get(self.api + f"stock/chart/line/intraday/{stock}/")
+    # def test_api_intradayChart(self):
+    #     for i, stock in zip(range(len(self.stocks)),self.stocks):
+    #         with self.subTest(i=i):
+    #             resp = requests.get(self.api + f"stock/chart/line/intraday/{stock}/")
 
-            # Check HTTP status code
-            self.assertEqual(resp.status_code, 200)
+    #             # Check HTTP status code
+    #             self.assertEqual(resp.status_code, 200)
 
-            # Check that it returns more than 50 entries
-            self.assertGreater(len(resp.json()), 50)
+    #             # Check that it returns more than 50 entries
+    #             self.assertGreater(len(resp.json()), 50)
 
-            # Check the formatting of the first 10 entries and spacing between 1 minute of each other
-            for i in range(10):
-                entry = resp.json()[i]['fields']
+    #             # Check the formatting of the first 10 entries and spacing between 1 minute of each other
+    #             for i in range(10):
+    #                 entry = resp.json()[i]['fields']
 
-                # Check keys in each entry
-                entry_keys = list(entry.keys())
-                self.assertEqual(entry_keys, ['stock_id', 'date', 'open', 'high', 'low', 'close', 'volume'])
+    #                 # Check keys in each entry
+    #                 entry_keys = list(entry.keys())
+    #                 self.assertEqual(entry_keys, ['stock_id', 'date', 'open', 'high', 'low', 'close', 'volume'])
 
-                # Check date formatting
-                entry_date_str = entry['date']
-                entry_date = datetime.strptime(entry_date_str, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc)
+    #                 # Check date formatting
+    #                 entry_date_str = entry['date']
+    #                 entry_date = datetime.strptime(entry_date_str, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc)
 
-                # Calculate the time difference between consecutive entries
-                if i > 0:
-                    prev_entry_date_str = resp.json()[i - 1]['fields']['date']
-                    prev_entry_date = datetime.strptime(prev_entry_date_str, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc)
-                    time_difference = entry_date - prev_entry_date
+    #                 # Calculate the time difference between consecutive entries
+    #                 if i > 0:
+    #                     prev_entry_date_str = resp.json()[i - 1]['fields']['date']
+    #                     prev_entry_date = datetime.strptime(prev_entry_date_str, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc)
+    #                     time_difference = entry_date - prev_entry_date
 
-                    # Check if the time difference is approximately 2 minutes
-                    self.assertLessEqual(time_difference, timedelta(minutes=2), f'{prev_entry_date_str} is more than 2 minutes from {entry_date_str} in request for {stock}')
+    #                     # Check if the time difference is approximately 2 minutes
+    #                     self.assertLessEqual(time_difference, timedelta(minutes=2), f'{prev_entry_date_str} is more than 2 minutes from {entry_date_str} in request for {stock}')
         
     
 
     def test_api_stockOverview(self):
         
-        for stock_symbol in self.stocks:
-            resp = requests.get(self.api + f"stock/overview/{stock_symbol}/")
+        for i, stock_symbol in zip(range(len(self.stocks)),self.stocks):
+            with self.subTest(i=i):
+                resp = requests.get(self.api + f"stock/overview/{stock_symbol}/")
 
-            # Check HTTP status code
-            self.assertEqual(resp.status_code, 200)
+                # Check HTTP status code
+                self.assertEqual(resp.status_code, 200, f'Response code for stock {stock_symbol} was {resp.status_code}')
 
-            # Check the structure of the response
-            expected_keys = ['model', 'pk', 'fields']
-            self.assertEqual(list(resp.json().keys()), expected_keys)
+                # Check the structure of the response
+                expected_keys = ['model', 'pk', 'fields']
+                self.assertEqual(list(resp.json().keys()), expected_keys,f'Keys are unexpected in stock {stock_symbol}')
 
-            # Check the fields in the response
-            fields = resp.json()['fields']
+                # Check the fields in the response
+                fields = resp.json()['fields']
 
-            expected_fields = {
-                "symbol", "asset_type", "name", "description", "cik", "exchange", "currency", "country", "sector",
-                "industry", "address", "fiscal_year_end", "latest_quarter", "market_capitalization", "ebitda",
-                "pe_ratio", "peg_ratio", "book_value", "dividend_per_share", "dividend_yield", "eps",
-                "revenue_per_share_ttm", "profit_margin", "operating_margin_ttm", "return_on_assets_ttm",
-                "return_on_equity_ttm", "revenue_ttm", "gross_profit_ttm", "diluted_eps_ttm",
-                "quarterly_earnings_growth_yoy", "quarterly_revenue_growth_yoy", "analyst_target_price",
-                "trailing_pe", "forward_pe", "price_to_sales_ratio_ttm", "price_to_book_ratio",
-                "ev_to_revenue", "ev_to_ebitda", "beta", "week_52_high", "week_52_low", "day_50_moving_average",
-                "day_200_moving_average", "shares_outstanding", "dividend_date", "ex_dividend_date"
-            }
+                expected_fields = {
+                    "symbol", "asset_type", "name", "description", "cik", "exchange", "currency", "country", "sector",
+                    "industry", "address", "fiscal_year_end", "latest_quarter", "market_capitalization", "ebitda",
+                    "pe_ratio", "peg_ratio", "book_value", "dividend_per_share", "dividend_yield", "eps",
+                    "revenue_per_share_ttm", "profit_margin", "operating_margin_ttm", "return_on_assets_ttm",
+                    "return_on_equity_ttm", "revenue_ttm", "gross_profit_ttm", "diluted_eps_ttm",
+                    "quarterly_earnings_growth_yoy", "quarterly_revenue_growth_yoy", "analyst_target_price",
+                    "trailing_pe", "forward_pe", "price_to_sales_ratio_ttm", "price_to_book_ratio",
+                    "ev_to_revenue", "ev_to_ebitda", "beta", "week_52_high", "week_52_low", "day_50_moving_average",
+                    "day_200_moving_average", "shares_outstanding", "dividend_date", "ex_dividend_date"
+                }
 
-            self.assertSetEqual(set(fields.keys()), expected_fields)
+                self.assertSetEqual(set(fields.keys()), expected_fields,f'fields for stock {stock_symbol} are incorrect. ')
 
-            # Add additional checks for specific data values if needed
-            # For example, check if market_capitalization is an integer, etc.
+                # Add additional checks for specific data values if needed
+                # For example, check if market_capitalization is an integer, etc.
 
-            # Check the data type of market_capitalization
-            self.assertIsInstance(fields["market_capitalization"], int)
+                # Check the data type of market_capitalization
+                self.assertIsInstance(fields["market_capitalization"], int, f'Market capitilazition in stock {stock_symbol} should be an int')
+                floatKeys = {"pe_ratio", "peg_ratio", "book_value", "dividend_yield", "eps",
+                        "revenue_per_share_ttm", "profit_margin", "operating_margin_ttm",
+                        "return_on_assets_ttm", "return_on_equity_ttm", "diluted_eps_ttm",
+                        "quarterly_earnings_growth_yoy", "quarterly_revenue_growth_yoy",
+                        "analyst_target_price", "trailing_pe", "forward_pe", "price_to_sales_ratio_ttm",
+                        "price_to_book_ratio", "ev_to_revenue", "ev_to_ebitda", "beta",
+                        "week_52_high", "week_52_low", "day_50_moving_average", "day_200_moving_average"}
+                #these keys may be null as a certain company may not use dividends
+                maybeNullFloatKeys={'dividend_per_share','dividend_yield','dividend_date','ex_dividend_date'}
+                # Check that each value in the fields dictionary is not blank
+                for key, value in fields.items():
+                    self.assertNotEqual(value, '', f"Value for '{key}' should not be blank.")
+
+                    if key in floatKeys:
+                        try:
+                            float_value = float(value)
+                            # Additional checks or assertions can be added if needed
+                        except:
+                            if not(key in maybeNullFloatKeys): 
+                                self.fail(f"Value for '{key}' in stock {stock_symbol} should be convertible to float.")
 
    
 
