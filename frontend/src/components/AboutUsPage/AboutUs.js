686558a18273d5e1a6a00305e54bb387567f568b
Issa-73 <123043253+Issa-73@users.noreply.github.com>
2024-03-14 00:02:11 -0700

    finalizing the about us bage
    change some colors for the cards and the action when hoverring added the names and the github links

--- frontend/src/components/AboutUsPage/AboutUs.js
+++ frontend/src/components/AboutUsPage/AboutUs.js
@@ -8,7 +8,7 @@ const people = [
     { id: 3, name: 'Steiner Christensen', github: 'https://github.com/SteinerChris' },
     { id: 4, name: 'Angel Lopez', github: 'https://github.com/AngelL-015' },
     { id: 5, name: 'Jeff Spinner', github: 'https://github.com/SpinnerJ' },
-    { id: 6, name: 'Issa', github: 'https://github.com/Issa-73' }
+    { id: 6, name: 'Ahmed Issa', github: 'https://github.com/Issa-73' }
 ];
 
 const AboutUs = () => {
@@ -18,9 +18,14 @@ const AboutUs = () => {
             <div className="about-us-container">
                 <h1>About The Project</h1>
                 <div className="project-description">
-                    <p>Welcome to our finance project!</p>
-                    <p>Here's a brief overview of what we do...</p>
-                    {/* Add more project description here as needed */}
+                
+        <p>Welcome to our finance project!, our Financial web service  app  aims to enhance the user experience in </p>
+        <p>accessing and analyzing stock market data. The project enable users to view live and historical data on  </p>
+        <p> various stocks, interact with dynamic graphs,and obtain detailed company information, all within a  </p>
+        <p>user-friendly web interface. The system will leverage a remote MySQL database for data storage and </p>
+        <p> employ Python and Django for backend operations, with React powering the frontend.</p>
+   
+                  
                 </div>
                 <h2>Our Team</h2>
                 <div className="people-container">
